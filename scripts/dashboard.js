6e75ce37ef515c3308eb14ac7e15daba6e268c81
Pengur <wojciechowski.jakub05@gmial.com>
2026-06-09 14:59:18 +0200

    small improvements

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -11,3 +11,43 @@ function timer() {
 window.onload = function() {
   timer();
 };
+
+function changeCamera(btn) {
+  var srcDiv = document.getElementById("cam-view");
+  var player = document.getElementById("video-player");
+  if (btn.id == "cam1")
+    srcDiv.src = "media/placeholder1.mp4";
+  if (btn.id == "cam2")
+    srcDiv.src = "media/placeholder2.mp4";
+  if (btn.id == "cam3")
+    srcDiv.src = "media/placeholder3.mp4";
+
+  player.load();
+  player.play();
+}
+
+function endGame() {
+  window.location.href = "index.html";
+}
+
+function reportProblem() {
+  window.location.href = "report.html";
+}
+
+let muted = true;
+function toggleMute() {
+  var audio;
+  var btn = document.getElementById("toggle-mic");
+  if (muted == true) {
+    audio = new Audio("media/micActive.mp3");
+    audio.play();
+    btn.textContent = "Wyłącz mikrofon";
+    muted = false;
+  }
+  else {
+    audio = new Audio("media/micMuted.mp3");
+    audio.play();
+    btn.textContent = "Włącz mikrofon";
+    muted = true;
+  }
+}
