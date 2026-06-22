87681d95b54725e68f80c73a538d6bc08f9a7fa8
lukas <260795@edu.p.lodz.pl>
2026-06-22 12:52:41 +0200

    report

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -33,7 +33,9 @@ function endGame() {
 function reportProblem() {
   window.location.href = "report.html";
 }
-
+function dashboard() {
+  window.location.href = "dashboard.html";
+}
 let muted = true;
 function toggleMute() {
   var audio;
